02010b39aa49d0eac859764313ada328493c2e69
Geoff Shannon <earthlingzephyr@gmail.com>
2011-03-16 20:14:02 -0700

    Updated color theme to be easier to see

--- lisp/ez-dark.el
+++ lisp/ez-dark.el
@@ -7,7 +7,7 @@
   (color-theme-install
    '(color-theme-ez-dark
        ((foreground-color . "#b9d3cf")
-         (background-color . "#2e3436") 
+         (background-color . "#232829") 
          (background-mode . dark))
        (bold ((t (:bold t))))
        (bold-italic ((t (:italic t :bold t))))
@@ -15,13 +15,13 @@
 
        (font-lock-builtin-face ((t (:foreground "#0c7deb"))))
        (font-lock-comment-face ((t (:italic t :foreground "#c08a20"))))
-       (font-lock-comment-delimiter-face ((t (:foreground "#bc020e"))))
+       (font-lock-comment-delimiter-face ((t (:foreground "#ed0212"))))
        (font-lock-constant-face ((t (:bold t :foreground "#729fd0"))))
        (font-lock-doc-string-face ((t (:foreground "#c08a20"))))
        (font-lock-doc-face ((t (:foreground "gray"))))
        (font-lock-reference-face ((t (:foreground "white"))))
        (font-lock-function-name-face ((t (:bold t :foreground "#458db0"))))
-       (font-lock-keyword-face ((t (:bold t :foreground "#bf1322"))))
+       (font-lock-keyword-face ((t (:bold t :foreground "#ef1322"))))
        (font-lock-preprocessor-face ((t (:foreground "#e3ea94"))))
        (font-lock-string-face ((t (:foreground "#aca000"))))
        (font-lock-type-face ((t (:bold t :foreground "#77608c"))))
@@ -33,4 +33,4 @@
        (region ((t (:foreground nil :background "#555555"))))
        (show-paren-match-face ((t (:bold t :foreground "#ffffff" 
                                     :background "#050505")))))))
-(color-theme-ez-dark)
+;(color-theme-ez-dark)
