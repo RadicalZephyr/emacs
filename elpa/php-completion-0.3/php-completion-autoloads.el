5e9155ae702b5309cee054fd0c94f9e8425a10bd
Geoff Shannon <earthlingzephyr@gmail.com>
2012-11-27 11:23:36 -0800

    Remove elpa files from git

--- elpa/php-completion-0.3/php-completion-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; php-completion-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("php-completion-pkg.el" "php-completion.el")
-;;;;;;  (20177 31595 253345))
-
-;;;***
-
-(provide 'php-completion-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; php-completion-autoloads.el ends here
