90c9c90b466f2abcb9e4ee3d51154902146f7068
Geoff Shannon <earthlingzephyr@gmail.com>
2013-04-16 16:47:11 -0700

    Fix some whitespace errors

--- lisp/tools.el
+++ lisp/tools.el
@@ -54,20 +54,20 @@ vi style of % jumping to matching brace."
 
 ;; someday might want to rotate windows if more than 2 of them
 (defun swap-windows ()
-  "If you have 2 windows, it swaps them." 
-  (interactive) 
-  (cond ((not (= (count-windows) 2)) 
-	 (message "You need exactly 2 windows to do this.")) 
-	(t (let* ((w1 (first (window-list)))
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((not (= (count-windows) 2))
+   (message "You need exactly 2 windows to do this."))
+  (t (let* ((w1 (first (window-list)))
               (w2 (second (window-list)))
               (b1 (window-buffer w1))
               (b2 (window-buffer w2))
               (s1 (window-start w1))
               (s2 (window-start w2)))
-	     (set-window-buffer w1 b2)
-	     (set-window-buffer w2 b1)
-	     (set-window-start w1 s2)
-	     (set-window-start w2 s1))))
+       (set-window-buffer w1 b2)
+       (set-window-buffer w2 b1)
+       (set-window-start w1 s2)
+       (set-window-start w2 s1))))
          (other-window 1))
 
 (defun rotate-windows ()
@@ -77,7 +77,7 @@ vi style of % jumping to matching brace."
          (message "You need more than one window to do this."))
 ;        ((= (count-windows) 2)
 ;         (swap-windows))
-        (t (switch-to-buffer (window-buffer 
+        (t (switch-to-buffer (window-buffer
                    (car (last (window-list)))))
            (dotimes (n (- (length (window-list)) 1) nil)
              (other-window 1)
@@ -90,33 +90,33 @@ vi style of % jumping to matching brace."
 (defun rename-file-and-buffer (new-name)
   "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
   (let ((name (buffer-name))
-	(filename (buffer-file-name)))
+  (filename (buffer-file-name)))
     (if (not filename)
-	(message "Buffer '%s' is not visiting a file!" name)
+  (message "Buffer '%s' is not visiting a file!" name)
       (if (get-buffer new-name)
-	  (message "A buffer named '%s' already exists!" new-name)
-	(progn 	 (rename-file name new-name 1) 	 
-		 (rename-buffer new-name) 	 
-		 (set-visited-file-name new-name) 	 
-		 (set-buffer-modified-p nil))))))
+    (message "A buffer named '%s' already exists!" new-name)
+  (progn 	 (rename-file name new-name 1)
+     (rename-buffer new-name)
+     (set-visited-file-name new-name)
+     (set-buffer-modified-p nil))))))
 
 ;; Never understood why Emacs doesn't have this function, either.
 ;;
 (defun move-buffer-file (dir)
   "Moves both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
  (let* ((name (buffer-name))
-	(filename (buffer-file-name))
-	(dir
-	  (if (string-match dir "\\(?:/\\|\\\\)$")
-	     (substring dir 0 -1) dir))
-	 (newname (concat dir "/" name)))
-   
+  (filename (buffer-file-name))
+  (dir
+    (if (string-match dir "\\(?:/\\|\\\\)$")
+       (substring dir 0 -1) dir))
+   (newname (concat dir "/" name)))
+
    (if (not filename)
      (message "Buffer '%s' is not visiting a file!" name)
  (progn (copy-file filename newname 1) (delete-file
-					filename) 
-	(set-visited-file-name newname) (set-buffer-modified-p
-					 nil) t))))
+          filename)
+  (set-visited-file-name newname) (set-buffer-modified-p
+           nil) t))))
 
 (defun print-to-pdf ()
   (interactive)
